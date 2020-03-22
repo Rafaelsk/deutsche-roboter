be9c9081638d7d51f12f9bdea43527e2cf43c9ff
Rafael Robles <megaprog@gmail.com>
2020-03-22 17:03:38 +0100

    First project

---
+++ src/main/java/org/zeitgeistonline/deutschesiri/DeutscheSiriApplication.java
@@ -0,0 +1,13 @@
+package org.zeitgeistonline.deutschesiri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeutscheSiriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeutscheSiriApplication.class, args);
+	}
+
+}
